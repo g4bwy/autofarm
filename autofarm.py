707ddbd9f1219c7748a520f150f984ed1013dabc
Gabriel FORTE <gforte@wyplay.com>
2021-02-22 11:27:50 +0100

    add generic vault support (XXX pancakeswap only)

--- autofarm.py
+++ autofarm.py
@@ -8,8 +8,9 @@ from web3.middleware import geth_poa_middleware
 
 try:
     my_addr = sys.argv[1]
+    vault_addr = sys.argv[2]
 except:
-    print("Usage: %s <BEP20 wallet address>" % sys.argv[0])
+    print("Usage: %s <wallet address> <vault address>" % sys.argv[0])
     sys.exit(-1)
 
 w3 = Web3(Web3.HTTPProvider('https://bsc-dataseed.binance.org/'))
@@ -22,35 +23,43 @@ autofarm_v2_addr = '0x0895196562C7868C5Be92459FaE7f877ED450452'
 autofarm_v2_abi = json.load(open('./autofarm_v2_abi.json'))
 autofarm_v2 = w3.eth.contract(autofarm_v2_addr, abi=autofarm_v2_abi['result'])
 
-wbnb_auto_lp_vault_addr = '0x65168C89a16FBEd4e2e418D5245FF626Bd66874b'
-wbnb_auto_lp_vault_abi = json.load(open('./wbnb_auto_lp_vault_abi.json'))
-wbnb_auto_lp_vault = w3.eth.contract(wbnb_auto_lp_vault_addr, abi=wbnb_auto_lp_vault_abi['result'])
+vault_abi = json.load(open('./autofarm_vault_abi.json'))
+vault = w3.eth.contract(vault_addr, abi=vault_abi['result'])
 
-auto_token_addr = wbnb_auto_lp_vault.functions.AUTOAddress().call()
-auto_token_abi = json.load(open('./auto_token_abi.json'))
-auto_token = w3.eth.contract(auto_token_addr, abi=auto_token_abi['result'])
+erc20_token_abi = json.load(open('./erc20_token_abi.json'))
+pancake_token_abi = json.load(open('./pancake_token_abi.json'))
+
+auto_token_addr = vault.functions.AUTOAddress().call()
+auto_token = w3.eth.contract(auto_token_addr, abi=erc20_token_abi['result'])
+auto_symbol = auto_token.functions.symbol().call()
 auto_decimals = 10 ** (auto_token.functions.decimals().call())
 
-poolInfo = ["want", "allocPoint", "lastRewardBlock", "accAUTOPerShare", "strat"]
-userInfo = ["shares", "rewardDebt"]
+want_token_addr = vault.functions.wantAddress().call()
+
+try:
+    want_token = w3.eth.contract(want_token_addr, abi=pancake_token_abi['result'])
+    token0_addr = want_token.functions.token0().call()
+    token0 = w3.eth.contract(token0_addr, abi=erc20_token_abi['result'])
+    token1_addr = want_token.functions.token1().call()
+    token1 = w3.eth.contract(token1_addr, abi=erc20_token_abi['result'])
+    want_symbol = "%s-%s LP" % (token1.functions.symbol().call(), token0.functions.symbol().call())
+except:
+    want_token = w3.eth.contract(want_token_addr, abi=erc20_token_abi['result'])
+    want_symbol = want_token.functions.symbol().call()
+
+want_decimals = 10 ** (want_token.functions.decimals().call())
 
 for i in range(0, autofarm_v2.functions.poolLength().call()):
     info = autofarm_v2.functions.poolInfo(i).call()
-    if info[4] == wbnb_auto_lp_vault_addr:
-        wbnb_auto_lp_vault_pid = i
+    if info[4] == vault_addr:
+        vault_pid = i
         break
 
 while True:
-    wbnb_auto_lp_vault_info = dict(zip(poolInfo, autofarm_v2.functions.poolInfo(wbnb_auto_lp_vault_pid).call()))
+    amount = autofarm_v2.functions.stakedWantTokens(vault_pid, my_addr).call() / float(want_decimals)
+    print("staked %s amount: %f" % (want_symbol, amount))
 
-    print("wbnb_auto_lp_vault: pid=%d, info=%s" % (wbnb_auto_lp_vault_pid, wbnb_auto_lp_vault_info))
-    my_info = dict(zip(userInfo, autofarm_v2.functions.userInfo(wbnb_auto_lp_vault_pid, my_addr).call()))
-    print(my_info)
-
-    amount = (my_info['shares'] / wbnb_auto_lp_vault.functions.sharesTotal().call() * wbnb_auto_lp_vault.functions.wantLockedTotal().call()) / float(auto_decimals)
-    print("AUTO amount: %f" % amount)
-
-    pending = autofarm_v2.functions.pendingAUTO(wbnb_auto_lp_vault_pid, my_addr).call() / float(auto_decimals)
-    print("pending AUTO reward: %f" % pending)
+    pending = autofarm_v2.functions.pendingAUTO(vault_pid, my_addr).call() / float(auto_decimals)
+    print("pending %s reward: %f" % (auto_symbol, pending))
 
     time.sleep(60)
